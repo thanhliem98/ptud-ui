9b0e27fc03d87113b8bf641e54d776477a4e4d2f
LL <liembsbs123@gmail.com>
2019-11-12 22:06:44 +0700

    done asset list

--- src/app/pages/assets/asset-list/asset-list.component.ts
+++ src/app/pages/assets/asset-list/asset-list.component.ts
@@ -7,6 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssetListComponent implements OnInit {
 
+  assets: Array<any>  = [
+    {
+      apartmentId: "CH000001",
+      category: "Tủ lạnh",
+      buyDate: "19/12/2019",
+      guaranteeTime: 36
+    },
+    {
+      apartmentId: "CH000002",
+      category: "Tủ lạnh",
+      buyDate: "19/12/2019",
+      guaranteeTime: 36
+    },
+    {
+      apartmentId: "CH000003",
+      category: "Tủ lạnh",
+      buyDate: "19/12/2019",
+      guaranteeTime: 36
+    },
+    {
+      apartmentId: "CH000004",
+      category: "Tủ lạnh",
+      buyDate: "19/12/2019",
+      guaranteeTime: 36
+    },
+    {
+      apartmentId: "CH000005",
+      category: "Tủ lạnh",
+      buyDate: "19/12/2019",
+      guaranteeTime: 36
+    },
+    {
+      apartmentId: "CH000006",
+      category: "Tủ lạnh",
+      buyDate: "19/12/2019",
+      guaranteeTime: 36
+    },
+    {
+      apartmentId: "CH000007",
+      category: "Tủ lạnh",
+      buyDate: "19/12/2019",
+      guaranteeTime: 36
+    },
+    {
+      apartmentId: "CH000008",
+      category: "Tủ lạnh",
+      buyDate: "19/12/2019",
+      guaranteeTime: 36
+    },
+    {
+      apartmentId: "CH000009",
+      category: "Tủ lạnh",
+      buyDate: "19/12/2019",
+      guaranteeTime: 36
+    },
+    {
+      apartmentId: "CH000010",
+      category: "Tủ lạnh",
+      buyDate: "19/12/2019",
+      guaranteeTime: 36
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
